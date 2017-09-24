be42d2285595ca9b89af78c44a0760741595cb5c
vaakinmade <vickeyakinmade22@gmail.com>
2017-09-24 20:02:44 +0100

    Changed url to support API generic view.

--- product/urls.py
+++ product/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.ListCreateProduct.as_view(), name="product_list"),
+	url(r'(?P<pk>\d+)/$', views.RetrieveUpdateDestroyProduct.as_view(),
+		name='product_detail')
 ]
