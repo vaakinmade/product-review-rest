515342e603149a2132236e6ceb4910e4873ec868
vaakinmade <vickeyakinmade22@gmail.com>
2017-09-24 20:04:47 +0100

    Chnanged APIView to generics ListCreateView. Implemented RetrieveUpdateDestroyView.

--- product/views.py
+++ product/views.py
@@ -1,19 +1,14 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import generics
 
 from . import models
 from . import serializers
 
 
-class ListCreateProduct(APIView):
-	def get(self, request, format=None):
-		products = models.Product.objects.all()
-		serializer = serializers.ProductSerializer(products, many=True)
-		return Response(serializer.data)
+class ListCreateProduct(generics.ListCreateAPIView):
+	queryset = models.Product.objects.all()
+	serializer_class = serializers.ProductSerializer
 
-	def post(self, request, format=None):
-		serializer = serializers.ProductSerializer(data=request.data)
-		serializer.is_valid(raise_exception=True)
-		serializer.save()
-		return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+class RetrieveUpdateDestroyProduct(generics.RetrieveUpdateDestroyAPIView):
+	queryset = models.Product.objects.all()
+	serializer_class = serializers.ProductSerializer
