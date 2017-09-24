b3acd3e3d60b70a0a9485e8f77da431530dbcfd9
vaakinmade <vickeyakinmade22@gmail.com>
2017-09-24 20:50:23 +0100

    Implemented review fetching for CRUD using product_pk. Wrote functionality to prevent submitting review for product other than the one selected.

--- product/views.py
+++ product/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from django.shortcuts import get_object_or_404
 
 from . import models
 from . import serializers
@@ -12,3 +13,28 @@ class ListCreateProduct(generics.ListCreateAPIView):
 class RetrieveUpdateDestroyProduct(generics.RetrieveUpdateDestroyAPIView):
 	queryset = models.Product.objects.all()
 	serializer_class = serializers.ProductSerializer
+
+
+class ListCreateReview(generics.ListCreateAPIView):
+	queryset = models.Review.objects.all()
+	serializer_class = serializers.ReviewSerializer
+
+	def get_queryset(self):
+		return self.queryset.filter(product_id=self.kwargs.get('product_pk'))
+
+	def perform_create(self, serializer):
+		product = get_object_or_404(
+			models.Product, pk=self.kwargs.get('product_pk'))
+		serializer.save(product=product)
+
+
+class RetrieveUpdateDestroyReview(generics.RetrieveUpdateDestroyAPIView):
+	queryset = models.Review.objects.all()
+	serializer_class = serializers.ReviewSerializer
+
+	def get_object(self):
+		return get_object_or_404(
+			self.get_queryset(),
+			product_id=self.kwargs.get('product_pk'),
+			pk=self.kwargs.get('pk')
+		)
