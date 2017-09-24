b33906a8ed85973e72e23ceb64d58bfef1c0033c
vaakinmade <vickeyakinmade22@gmail.com>
2017-09-24 16:01:11 +0100

    Implemented API get/post functionality with model serializers.

--- product/views.py
+++ product/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from . import models
+from . import serializers
+
+
+class ListCreateProduct(APIView):
+	def get(self, request, format=None):
+		products = models.Product.objects.all()
+		serializer = serializers.ProductSerializer(products, many=True)
+		return Response(serializer.data)
+
+	def post(self, request, format=None):
+		serializer = serializers.ProductSerializer(data=request.data)
+		serializer.is_valid(raise_exception=True)
+		serializer.save()
+		return Response(serializer.data, status=status.HTTP_201_CREATED)
