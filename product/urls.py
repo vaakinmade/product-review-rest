ac6390196de613aaa9096bf11edc000e7fc8849b
vaakinmade <vickeyakinmade22@gmail.com>
2017-09-24 20:47:08 +0100

    Added url route for product reviews CRUD functionality.

--- product/urls.py
+++ product/urls.py
@@ -4,6 +4,11 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.ListCreateProduct.as_view(), name="product_list"),
-	url(r'(?P<pk>\d+)/$', views.RetrieveUpdateDestroyProduct.as_view(),
-		name='product_detail')
+	url(r'^(?P<pk>\d+)/$', views.RetrieveUpdateDestroyProduct.as_view(),
+		name='product_detail'),
+	url(r'^(?P<product_pk>\d+)/reviews/$', views.ListCreateReview.as_view(),
+		name='review_list'),
+	url(r'^(?P<product_pk>\d+)/reviews/(?P<pk>\d+)/$',
+		views.RetrieveUpdateDestroyReview.as_view(),
+		name='review_detail'),
 ]
