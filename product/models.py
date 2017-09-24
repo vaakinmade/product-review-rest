81a48b29802a1baa39077108bc14dd592e9eea40
vaakinmade <vickeyakinmade22@gmail.com>
2017-09-24 14:44:24 +0100

    Added product and review model classes.

--- product/models.py
+++ product/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+	title = models.CharField(max_length=255)
+	url = models.URLField(unique=True)
+
+	def __str__(self):
+		return self.title
+
+
+class Review(models.Model):
+	product = models.ForeignKey(Product, related_name="reviews")
+	name = models.CharField(max_length=255)
+	email = models.EmailField()
+	comment = models.TextField(blank=True, default="")
+	rating = models.IntegerField()
