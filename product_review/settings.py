af1af8bbb71c34607742d85fd7e4bc41aee5fbc3
vaakinmade <vickeyakinmade22@gmail.com>
2017-09-25 02:05:50 +0100

    Added pagination entry in rest_framework settings.

--- product_review/settings.py
+++ product_review/settings.py
@@ -129,5 +129,8 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
-        )
+        ),
+    'DEFAULT_PAGINATION_CLASS':
+    'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 1,
 }
