9262e73dddf5317c80acb0c7aec745e7ecbfe66b
vaakinmade <vickeyakinmade22@gmail.com>
2017-09-26 02:52:17 +0100

    Added custom permission class to authorize superuser action and prevent unauthorised deletion.

---
+++ product/permissions.py
@@ -0,0 +1,9 @@
+from rest_framework import permissions
+
+
+class IsSuperUser(permissions.BasePermission):
+	def has_permission(self, request, view):
+		if request.user.is_superuser:
+			return True
+		elif request.method == 'DELETE':
+			return False
